004123c49f46ff619e22f8d0cc750d8cf0b5f6ff
Marquavious Draggon <marq.draggon@gmail.com>
2017-04-03 14:43:25 -0700

    cool, done with hastabels

--- source/hashtable.py
+++ source/hashtable.py
@@ -27,6 +27,7 @@ class HashTable(object):
         """Return the load factor, the ratio of number of entries to buckets"""
         # TODO: Calculate load factor
         # return ...
+        return (self.size / float(len(self.buckets)))
 
     def _resize(self, new_size=None):
         """Resize this hash table's buckets and rehash all key-value entries.
@@ -39,12 +40,19 @@ class HashTable(object):
         elif new_size is 0:
             new_size = self.size / 2  # Half size
         # TODO: Get a list to temporarily hold all current key-value entries
-        # ...
+        new_items = self.items()
         # TODO: Create a new list of new_size total empty linked list buckets
+
+        self.buckets = [LinkedList() for i in range(new_size)]
+        self.size = 0
+        # return new_size
         # ...
         # TODO: Insert each key-value entry into the new list of buckets,
         # which will rehash them into a new bucket index based on the new size
         # ...
+        for key, item in new_items:
+            # self.buckets[key] = item
+            self.set(key,item)
 
     def keys(self):
         """Return a list of all keys in this hash table"""
@@ -118,10 +126,14 @@ class HashTable(object):
             # In this case, the given key's value is being updated
             # Remove the old key-value entry from the bucket first
             bucket.delete(entry)
+        else:
+            self.size += 1
         # Insert the new key-value entry into the bucket in either case
         bucket.append((key, value))
+
         # TODO: Check if the load factor exceeds a threshold such as 0.75
-        # ...
+        if self.load_factor() > 0.75:
+            self._resize()
         # TODO: If so, automatically resize to reduce the load factor
         # ...
 
@@ -135,9 +147,9 @@ class HashTable(object):
         if entry is not None:  # Found
             # Remove the key-value entry from the bucket
             bucket.delete(entry)
+            self.size -= 1
         else:  # Not found
             raise KeyError('Key not found: {}'.format(key))
-
 
 def test_hash_table():
     ht = HashTable(4)
